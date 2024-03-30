6e8d95ca5e24d65ea3a3c172a31bbd9cce4011ff
eslam <ealaa3764@gmail.com>
2024-03-30 06:13:23 +0200

    fuck

--- db/dbready.js
+++ db/dbready.js
@@ -3,96 +3,102 @@ const client = require("./db");
 async function isReady() {
   try {
     const tableCheckQuery = `
-        SELECT EXISTS (
-          SELECT 1
-          FROM information_schema.tables
-          WHERE table_name = $1
-        );
-      `;
+      SELECT EXISTS (
+        SELECT 1
+        FROM information_schema.tables
+        WHERE table_name = $1
+      );
+    `;
 
     const createTableQueries = [
-      `CREATE TABLE classes (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(255) NOT NULL
-        );
-        INSERT INTO classes (name) VALUES ('Excursions from Hurghada');
-        INSERT INTO classes (name) VALUES ('Excursions from Marsa Alam');
-        INSERT INTO classes (name) VALUES ('Excursions from El-Ouseir');
-        INSERT INTO classes (name) VALUES ('Safaga Excursions');
-        `,
-      `CREATE TABLE IF NOT EXISTS images (
-        image_id VARCHAR(255) PRIMARY KEY,
+      `
+      CREATE TABLE IF NOT EXISTS classes (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+      );
+      `,
+      `
+      CREATE TABLE IF NOT EXISTS images (
+        image_id SERIAL PRIMARY KEY,
         image VARCHAR(255) NOT NULL
       );
       `,
-      `CREATE TABLE IF NOT EXISTS blogs (
-          id VARCHAR(255) PRIMARY KEY,
-          description VARCHAR(1000) NOT NULL,
-          date VARCHAR(255) NOT NULL 
-        );
+      `
+      CREATE TABLE IF NOT EXISTS blogs (
+        id SERIAL PRIMARY KEY,
+        description VARCHAR(1000) NOT NULL,
+        date DATE NOT NULL 
+      );
       `,
-      `CREATE TABLE IF NOT EXISTS trips (
-            id VARCHAR(255) PRIMARY KEY,
-            name VARCHAR(300),
-            price INT NOT NULL,
-            vehicle VARCHAR(300),
-            duration VARCHAR(300),
-            guiding VARCHAR(300),
-            description VARCHAR(1500) NOT NULL,
-            image VARCHAR(300) NOT NULL,
-            active BOOLEAN DEFAULT true,
-            video VARCHAR(500) NOT NULL
-        )        
-          `,
-      `CREATE TABLE IF NOT EXISTS trips_type (
-           trip_id VARCHAR(255) REFERENCES trips(id) ON DELETE CASCADE,
-           type INT REFERENCES classes(id) ON DELETE CASCADE
-          )  ;
-          `,
-      `CREATE TABLE IF NOT EXISTS contactus (
-          id SERIAL PRIMARY KEY,
-          name VARCHAR(255) NOT NULL,
-          mail VARCHAR(255) NOT NULL ,
-          description VARCHAR(600) NOT NULL 
-          );
-        `,
-      `CREATE TABLE IF NOT EXISTS accounts (
-          id SERIAL PRIMARY KEY,
-          name VARCHAR(255) NOT NULL,
-          mail VARCHAR(255) NOT NULL UNIQUE ,
-          role VARCHAR(255) NOT NULL DEFAULT 'user',
-          pass VARCHAR(255) NOT NULL ,
-          verify_code VARCHAR(255)
-        );
-        `,
-      `CREATE TABLE IF NOT EXISTS orders (
-          id SERIAL PRIMARY KEY,
-          user_id INT REFERENCES accounts(id) NOT NULL,
-          name VARCHAR(255) NOT NULL,
-          trip_id VARCHAR(255) REFERENCES trips(id) NOT NULL,
-          number_of_person INT NOT NULL,
-          arrivaldate VARCHAR(255) NOT NULL,
-          departuredate VARCHAR(255) NOT NULL,
-          flight_number VARCHAR(255) NOT NULL,
-          hotel_name VARCHAR(255) NOT NULL,
-          room_num VARCHAR(255) NOT NULL,
-          paid BOOLEAN DEFAULT FALSE,
-          o_id VARCHAR(255) 
-         );
-        `,
-      `CREATE TABLE IF NOT EXISTS feedbacks (
-          id SERIAL PRIMARY KEY,
-          user_id INT REFERENCES accounts(id) NOT NULL,
-          comment VARCHAR(255) ,
-          video VARCHAR(500) 
-         );
-        `,
-        `
-        CREATE TABLE IF NOT EXISTS accountactive (
-          id SERIAL PRIMARY KEY,
-          user_id INT REFERENCES accounts(id) NOT NULL
-          );
-        `
+      `
+      CREATE TABLE IF NOT EXISTS trips (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(300),
+        price INT NOT NULL,
+        vehicle VARCHAR(300),
+        duration VARCHAR(300),
+        guiding VARCHAR(300),
+        description VARCHAR(1500) NOT NULL,
+        image VARCHAR(300) NOT NULL,
+        active BOOLEAN DEFAULT true,
+        video VARCHAR(500) NOT NULL
+      );
+      `,
+      `
+      CREATE TABLE IF NOT EXISTS trips_type (
+        id SERIAL PRIMARY KEY,
+        trip_id INT REFERENCES trips(id) ON DELETE CASCADE,
+        type INT REFERENCES classes(id) ON DELETE CASCADE
+      );
+      `,
+      `
+      CREATE TABLE IF NOT EXISTS contactus (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        mail VARCHAR(255) NOT NULL,
+        description VARCHAR(600) NOT NULL
+      );
+      `,
+      `
+      CREATE TABLE IF NOT EXISTS accounts (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        mail VARCHAR(255) NOT NULL UNIQUE,
+        role VARCHAR(255) NOT NULL DEFAULT 'user',
+        pass VARCHAR(255) NOT NULL,
+        verify_code VARCHAR(255)
+      );
+      `,
+      `
+      CREATE TABLE IF NOT EXISTS orders (
+        id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES accounts(id) NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        trip_id INT REFERENCES trips(id) NOT NULL,
+        number_of_person INT NOT NULL,
+        arrivaldate DATE NOT NULL,
+        departuredate DATE NOT NULL,
+        flight_number VARCHAR(255) NOT NULL,
+        hotel_name VARCHAR(255) NOT NULL,
+        room_num VARCHAR(255) NOT NULL,
+        paid BOOLEAN DEFAULT FALSE,
+        o_id VARCHAR(255)
+      );
+      `,
+      `
+      CREATE TABLE IF NOT EXISTS feedbacks (
+        id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES accounts(id) NOT NULL,
+        comment VARCHAR(255),
+        video VARCHAR(500)
+      );
+      `,
+      `
+      CREATE TABLE IF NOT EXISTS accountactive (
+        id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES accounts(id) NOT NULL
+      );
+      `
     ];
 
     const tablesToCheck = [
